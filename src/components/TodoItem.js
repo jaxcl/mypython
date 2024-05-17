cf3a69ca3cfd9cc30d4212172a8acaae7e9319d1
EC2 Default User <ec2-user@ip-172-31-43-87.ec2.internal>
2024-05-17 16:25:18 +0000

    All project

---
+++ src/components/TodoItem.js
@@ -0,0 +1,14 @@
+import React from 'react';
+ 
+function TodoItem({ todo, onDelete }) {
+  return (
+    <li>
+      {todo}
+      <button onClick={onDelete}>Eliminar</button>
+    </li>
+  );
+}
+ 
+export default TodoItem;
+
+
